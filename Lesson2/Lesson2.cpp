53f094459d2096ff7ce7a0f44eceb2bd3579f1dc
Quliy_zc56 <Quliy_zc56@STHQ0305-13>
2024-07-02 21:46:51 +0400

    Add project files.

---
+++ Lesson2/Lesson2.cpp
@@ -0,0 +1,90 @@
+﻿
+#include <iostream>
+using namespace std;
+//
+//int main()
+//{
+//	cout << "Sairin adi:Eli Kerim" << endl;
+//	cout << "Seirin adi:Qayit\n" << endl;
+//	cout << "Hesretin araya atdi dag, dere," << endl;
+//	cout << "Senen isiq oldun, batan ses oldun,"<< endl;
+//	cout << "Qayit, menim gulum, qayit bu yere," << endl;
+//	cout << "Ey menim isteyim, ne gelmez oldun!" << endl;
+//  return 0;
+//
+//
+//
+//}
+
+//int main()
+//{
+//	cout << "|---------------------------|" << endl;
+//	cout << "|Pers Pisik balalari satiram|" << endl;
+//	cout << "|---------------------------|" << endl;
+//	cout << "|2 aydir doqulub            |" << endl;
+//	cout << "|Terbiyelidir               |" << endl;
+//	cout << "|Ustunde her seyi verilir   |" << endl;
+//	cout << "|Qiymet:300azn              |" << endl;
+// 	cout << "|tel:050-xxx-xx-xx          |" << endl;
+//	cout << "|---------------------------|" << endl;
+//}
+
+//int main()
+//{
+//	double R1 = 0;
+//	double R2 = 0;
+//	double R3 = 0;
+//
+//	cout << "R1:";
+//	cin >> R1;
+//	cout << "R2:";
+//	cin >> R2;
+//	cout << "R3:";
+//	cin >> R3;
+//	double R0 =1/(1 / R1 + 1 / R2 + 1 / R3);
+//	cout << "R0:" << R0;
+//
+//}
+//int main()
+//{
+//	double R = 0;
+//	double pi = 3.1;
+//	
+//
+//	cout << "R:";
+//	cin >> R;
+//	
+//	double S = pi * R * R;
+//	cout << "S:" << S;
+//	double L = 2 * (S);
+//	cout << "L:" << L;
+//
+//
+//}
+//int main()
+//{
+//	double v = 0;
+//	double t = 0;
+//
+//	cout << "V:";
+//	cin >> v;
+//	cout << "T:";
+//	cin >> t;
+//
+//	double S = v / t;
+//	cout << "S:" << S;
+//}
+int main()
+{
+	double s = 0;
+	double d = 30;
+	double sa = 0;
+
+	cout << "s:";
+	cin >> s;
+	cout << "sa:";
+	cin >> sa;
+
+	double call = s + d / 60 + sa / 3600;
+	cout << "call;" << call;
+}
